9e8116e01bb2b7092a8095ab500313aa6de32fcb
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 19:21:47 -0300

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -2,6 +2,10 @@ String.prototype.slice() -> (Method) Extracts a section of a string a returns it
                             Syntax: slice(beginIndex)
                                     slice(beginIndex, endIndex)
                             MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
-String.prototype.split() -> (Method) 
+String.prototype.split() -> (Method) Devides a string into an ordered list of substrings, puts these substrings into an array, and returns the array.
+                            Syntax: split()
+                                    split(separator)
+                                    split(separator, limit)
+                            MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 Number
