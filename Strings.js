2f31698518fefe287e8a41b410b591b7fb0382f9
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 08:43:34 -0300

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,5 @@
+String.prototype.slice() -> (Method) Extracts a section of a string a returns it as a new string, without modifying the original string.
+                            Syntax: slice(beginIndex)
+                                    slice(beginIndex, endIndex)
+                            MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+String.prototype.split() -> (Method) 
