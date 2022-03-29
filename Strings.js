90778f8e9772b364097c57a911f9afa0d308218f
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 19:28:33 -0300

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -1,11 +1,14 @@
-String.prototype.slice() -> (Method) Extracts a section of a string a returns it as a new string, without modifying the original string.
+String.prototype.slice() -> Extracts a section of a string a returns it as a new string, without modifying the original string.
                             Syntax: slice(beginIndex)
                                     slice(beginIndex, endIndex)
                             MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
-String.prototype.split() -> (Method) Devides a string into an ordered list of substrings, puts these substrings into an array, and returns the array.
+String.prototype.split() -> Devides a string into an ordered list of substrings, puts these substrings into an array, and returns the array.
                             Syntax: split()
                                     split(separator)
                                     split(separator, limit)
                             MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+Math.floor() -> Returns the largest integer less than or equal to a given number.
+                Syntax: Math.floor(x)
+                MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
 
-Number
+
