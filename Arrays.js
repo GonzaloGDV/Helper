ac103c15a3f39827f40b213379f8c3c6d1226420
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 19:31:24 -0300

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,3 @@
+Array.prototype.reverse() ->  Reverses an array in place.
+                              Syntax: reverse()
+                              MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
