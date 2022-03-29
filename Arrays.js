75b3d6042f41ecb9adcb4ef34b5150b7429f2ca0
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 19:32:43 -0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,3 +1,3 @@
-Array.prototype.reverse() ->  Reverses an array in place.
+Array.prototype.reverse() ->  Reverses an array in place. It changes the original array.
                               Syntax: reverse()
                               MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
