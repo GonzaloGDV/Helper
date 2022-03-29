299fdaef1e80768ef6c5e3bd67aa3916e299dbda
GonzaloGDV <69487420+GonzaloGDV@users.noreply.github.com>
2022-03-29 10:55:31 -0300

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -3,3 +3,5 @@ String.prototype.slice() -> (Method) Extracts a section of a string a returns it
                                     slice(beginIndex, endIndex)
                             MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
 String.prototype.split() -> (Method) 
+
+Number
